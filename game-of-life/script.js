74a8372f0b38451ef7a135ae7fc32b85a9e1734a
Pneuma <syca0714@gmail.com>
2021-01-21 23:24:29 +0900

    .

---
+++ game-of-life/script.js
@@ -0,0 +1,20 @@
+let gen = 0, isPaused = false
+
+let prevStat = new Array(10).fill(new Array(10).fill(0)), newStat = prevStat
+
+document.querySelector('#pause').addEventListener('click', () => isPaused = true)
+
+document.querySelector('#resume').addEventListener('click', () => isPaused = false)
+
+setInterval(() => {
+    if (isPaused) return
+
+    prevStat.forEach((row, i) => {
+        row.forEach((cell, j) => {
+            // TODO
+        })
+    })
+
+    gen += 1
+    document.querySelector('#gen').value = gen
+}, 100)
